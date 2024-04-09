416b3c4f7b4eab96a3dc21834380131244ee01c0
sandiaxcx <sandra8x@gmail.com>
2024-04-09 16:12:25 +0200

    removed solution

--- src/js/greetings.js
+++ src/js/greetings.js
@@ -1,26 +1,8 @@
 function greet(language) {
-    switch(language) {
-        case 'Australia':
-            return "Hello";
-        case 'Vietnam':
-            return "Xin Chào";
-        case 'Mexico':
-            return "Hola";
-        case 'Germany':
-            return "Hallo";
-        case 'Italy':
-            return "Ciao";
-        default:
-            return "Saluton";
-    }
+    
 }
 
-console.log(greet('Australia'));
-console.log(greet('Vietnam'));
-console.log(greet('Mexico'));
-console.log(greet('Germany'));
-console.log(greet('Italy'));
-console.log(greet('Unknown'));
+
 
 
 module.exports ={greet}
